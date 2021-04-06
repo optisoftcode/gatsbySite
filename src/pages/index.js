c7abf662886e4e8ba09f29ab6fbe29843e3e8691
optisoft <72044402+optisoftcode@users.noreply.github.com>
2021-04-06 15:00:09 +0530

    ss

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,39 +12,41 @@ const IndexPage = () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <div class="col-md-6">
-         <div class="form-group"><input placeholder="NAME" type="name" id="formBasicEmail" class="form-control"><i class="fa fa-user"></i></div>
+      <div className="col-md-6">
+         <div className="form-group"><input placeholder="NAME" type="name" id="formBasicEmail" className="form-control" /><i className="fa fa-user"></i></div>
       </div>
-      <div class="col-md-6">
-         <div class="form-group"><input placeholder="Telefon nummer" type="number" id="formBasicEmail" class="form-control"><i class="fa fa-phone"></i></div>
+      <div className="col-md-6">
+         <div className="form-group"><input placeholder="Telefon nummer" type="number" id="formBasicEmail" className="form-control" /><i className="fa fa-phone"></i></div>
       </div>
-      <div class="col-md-12">
-         <div class="form-group"><input placeholder="Email" type="email" id="formBasicEmail" class="form-control"><i class="fa fa-envelope"></i></div>
+      <div className="col-md-12">
+         <div className="form-group"><input placeholder="Email" type="email" id="formBasicEmail" className="form-control" /><i className="fa fa-envelope"></i></div>
       </div>
  
-      <div class="col-md-6">
-         <label class="text-uppercase form-label">Ønskede teetid</label>
-         <div class="form-group"><input placeholder="hh : mm" type="name" id="formBasicEmail" class="form-control"><i class="fa fa-pencil"></i></div>
+      <div className="col-md-6">
+         <label className="text-uppercase form-label">Ønskede teetid</label>
+         <div className="form-group"><input placeholder="hh : mm" type="name" id="formBasicEmail" className="form-control" /><i className="fa fa-pencil"></i></div>
       </div>
-      <div class="col-md-12">
-         <div class="form-group row">
-            <label class="text-uppercase form-label col-form-label col-6">Inklusiv transport?</label>
-            <div class="col-6">
-               <div class="radio-custom">
-                  <div class="form-check"><input name="formHorizontalRadios" type="radio" id="formHorizontalRadios1" class="form-check-input"><label title="" for="formHorizontalRadios1" class="form-check-label">Ja tak</label></div>
-                  <div class="form-check"><input name="formHorizontalRadios" type="radio" id="formHorizontalRadios2" class="form-check-input"><label title="" for="formHorizontalRadios2" class="form-check-label">Nej tak</label></div>
+      <div className="col-md-12">
+         <div className="form-group row">
+            <label className="text-uppercase form-label col-form-label col-6">Inklusiv transport?</label>
+            <div className="col-6">
+               <div className="radio-custom">
+                  <div className="form-check"><input name="formHorizontalRadios" type="radio" id="formHorizontalRadios1" className="form-check-input" /><label title="" for="formHorizontalRadios1" className="form-check-label">Ja tak</label></div>
+                  <div className="form-check"><input name="formHorizontalRadios" type="radio" id="formHorizontalRadios2" className="form-check-input" /><label title="" for="formHorizontalRadios2" className="form-check-label">Nej tak</label></div>
                </div>
             </div>
          </div>
       </div>
-      <div class="col-md-12">
-         <div class="form-group"><textarea rows="4" placeholder="Evt yderligere info?" id="formBasicEmail" class="form-control"></textarea><i class="fa fa-pencil"></i></div>
-         <button type="button" class="btn-orange-sendmg mr-auto ml-auto btn btn-primary">Send</button>
+      <div className="col-md-12">
+         <div className="form-group"><textarea rows="4" placeholder="Evt yderligere info?" id="formBasicEmail" className="form-control"></textarea><i className="fa fa-pencil"></i></div>
+         <button type="button" className="btn-orange-sendmg mr-auto ml-auto btn btn-primary">Send</button>
       </div>
 	  
 	  
 	  
- </form>
+    </form>
+
+
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
